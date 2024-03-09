a37f006145e3518f47e85313cf7f97b44abcfc9b
Niswatun Khasanah Dahtiar <146053422+niswakd@users.noreply.github.com>
2024-03-09 16:56:10 +0700

    Add files via upload

---
+++ tugas/home.php
@@ -0,0 +1,18 @@
+<!-- Task 1
+Pisah kode di bawah ke dalam file-file yang sudah dibuat sebelumnya. -->
+
+<?php
+    include_once 'header.php'
+?>
+				<h1>Welcome Home!</h1>
+				<p>
+				    <!--
+				        Tuliskan Nama - NIM - Rombel 
+				    -->
+
+                    Niswatun Khasanah Dahtiar - 0110123125 - 2SI02
+				</p>
+<?php
+    include_once 'footer.php'
+?>
+				
---
+++ tugas/libfungsi.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Task 4 - Function
+* function kelulusan
+* function grade
+*/
+
+	function kelulusan(){
+		/**
+		* Function kelulusan, jika:
+		* total nilai lebih dari 55 maka lulus
+		* total nilai kurang dari 55 maka tidak lulus
+		*/
+	}
+	
+	function grade() {
+	  /**
+		* Function grade, jika:
+		* total nilai <= 100 maka A
+		* total nilai <= 84 maka B
+		* total nilai <= 69 maka C
+		* total nilai <= 55 maka D
+		* total nilai <= 35 maka E
+		* selain itu maka I
+		*/ 
+	}
+?>

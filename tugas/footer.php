343dca1fe2d4c125c60f8f3617d67f580d1e1c44
Niswatun Khasanah Dahtiar <146053422+niswakd@users.noreply.github.com>
2024-03-10 22:32:08 +0700

    Add files via upload

---
+++ tugas/footer.php
@@ -0,0 +1,8 @@
+<!-- Tampilkan bagian footer di sini -->
+
+<footer class="text-center py-3 bg-light mt-5">
+        <p>Created by <a href="#">Niswatun Khasanah Dahtiar</a> &copy; <?php echo date('Y') ?></p>
+    </footer>
+</body>
+
+</html>
